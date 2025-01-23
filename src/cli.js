efc3e5c78b14d77a36229cdafd92b1cbd5da76f2
olga rozmetova <olga.rozmetova@gmail.com>
2025-01-24 00:37:02 +0100

    Create eslint config

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const getName = () => {
   const userName = readlineSync.question('May I have your name?');
-  console.log (`Hello, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 
 export default getName;
