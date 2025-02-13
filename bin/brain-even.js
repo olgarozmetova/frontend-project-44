61e076e9963aad7b9a6d26b26b279b0c2bb73ad3
olga rozmetova <olga.rozmetova@gmail.com>
2025-02-13 12:54:37 +0100

    Add for

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -9,43 +9,27 @@ const userName = getName();
 
 console.log('Answer "yes" if the number is even, otherwise answer "no". ');
 
-const startFirstGame = () => {
-  const question = getRandomNum();
-  console.log(`Question: ${question}`);
+const playGame = () => {
+  for (let i = 1; i < 4; i++) {
+    const question = getRandomNum();
+    console.log(`Question: ${question}`);
 
-  const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = isEven(question) ? 'yes' : 'no';
 
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
+    const userAnswer = readlineSync.question('Your answer: ');
 
-  if (correctAnswer !== userAnswer) {
-    console.log(
-      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
-Let's try again!, ${userName}!`
-    );
-  } else {
-    console.log('Correct!');
+    if (userAnswer === correctAnswer) {
+      console.log('Correct!');
+    } else {
+      console.log(
+        `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
+Let's try again, ${userName}!`
+      );
+      return;
+    }
   }
+
+  console.log(`Congratulations, ${userName}!`);
 };
 
-startFirstGame();
-
-/* const startFirstGame = () => {
-  const question = getRandomNum();
-  console.log(`Question: ${question}`);
-
-  const userAnswer = readlineSync.question('Your answer: ');
-
-  const correctAnswer = isEven(question) ? 'yes' : 'no';
-
-  if (correctAnswer !== userAnswer) {
-    console.log(
-      `'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'.
-Let's try again!, ${userName}!`
-    );
-  } else {
-    console.log('Correct!');
-  }
-};
-
-startFirstGame();
- */
+playGame();
