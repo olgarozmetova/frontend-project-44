13220ac3345c779a8399dd4bc478188812a9e485
olga rozmetova <olga.rozmetova@gmail.com>
2025-02-13 12:26:52 +0100

    Minor changes

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,10 @@
-import globals from "globals";
+import globals from 'globals';
 
-import path from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
-import pluginJs from "@eslint/js";
-import importPlugin from "eslint-plugin-import";
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import pluginJs from '@eslint/js';
+import importPlugin from 'eslint-plugin-import';
 
 // mimic CommonJS variables -- not needed if using CommonJS
 const __filename = fileURLToPath(import.meta.url);
@@ -24,8 +24,8 @@ export default [
       parserOptions: {
         // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
         // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
-        ecmaVersion: "latest",
-        sourceType: "module",
+        ecmaVersion: 'latest',
+        sourceType: 'module',
       },
     },
     plugins: { import: importPlugin },
@@ -33,25 +33,25 @@ export default [
       ...importPlugin.configs.recommended.rules,
     },
   },
-  ...compat.extends("airbnb-base"),
+  ...compat.extends('airbnb-base'),
   {
     rules: {
-      "no-underscore-dangle": [
-        "error",
+      'no-underscore-dangle': [
+        'error',
         {
-          allow: ["__filename", "__dirname"],
+          allow: ['__filename', '__dirname'],
         },
       ],
-      "import/extensions": [
-        "error",
+      'import/extensions': [
+        'error',
         {
-          js: "always",
+          js: 'always',
         },
       ],
-      "import/no-named-as-default": "off",
-      "import/no-named-as-default-member": "off",
-      "no-console": "off",
-      "import/no-extraneous-dependencies": "off",
+      'import/no-named-as-default': 'off',
+      'import/no-named-as-default-member': 'off',
+      'no-console': 'off',
+      'import/no-extraneous-dependencies': 'off',
     },
   },
 ];
--- src/cli.js
+++ src/cli.js
@@ -1,8 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const getName = () => {
-  const userName = readlineSync.question('May I have your name?');
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!`);
+  return userName;
 };
 
 export default getName;
