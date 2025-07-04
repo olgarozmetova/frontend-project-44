c35fd6ef33537f194a3fc0b7e7a5274cf17f63c3
olga rozmetova <olga.rozmetova@gmail.com>
2025-07-04 18:39:53 +0200

    added brain-gcd

---
+++ bin/brain-gcd.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+import playGcdGame from '../src/games/brain-gcd.js';
+
+playGcdGame();
---
+++ src/games/brain-gcd.js
@@ -0,0 +1,21 @@
+import app from '../index.js';
+import getRandomNum from '../randomNum.js';
+
+const questionDescr = 'Find the greatest common divisor of given numbers. ';
+
+function getData() {
+  let num1 = getRandomNum();
+  let num2 = getRandomNum();
+  const question = `${num1} ${num2}`;
+
+  while (num2 !== 0) {
+    const temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
+  }
+
+  const correctAnswer = num1;
+  return [question, correctAnswer];
+}
+
+export default () => app(questionDescr, getData);
