55b0d922efa4d11ad7fbf257d51a16e9d93712cf
olga rozmetova <olga.rozmetova@gmail.com>
2025-07-14 11:28:16 +0200

    modified linter

--- eslint.config.js
+++ eslint.config.js
@@ -1,57 +1,39 @@
 import globals from 'globals';
-
-import path from 'path';
-import { fileURLToPath } from 'url';
-import { FlatCompat } from '@eslint/eslintrc';
 import pluginJs from '@eslint/js';
-import importPlugin from 'eslint-plugin-import';
-
-// mimic CommonJS variables -- not needed if using CommonJS
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-  recommendedConfig: pluginJs.configs.recommended,
-});
+import * as tseslint from '@typescript-eslint/eslint-plugin';
+import parserTs from '@typescript-eslint/parser';
+import stylistic from '@stylistic/eslint-plugin';
 
 export default [
+  stylistic.configs.recommended,
+  pluginJs.configs.recommended,
   {
+    files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
+      parser: parserTs,
       parserOptions: {
-        // Eslint doesn't supply ecmaVersion in `parser.js` `context.parserOptions`
-        // This is required to avoid ecmaVersion < 2015 error or 'import' / 'export' error
-        ecmaVersion: 'latest',
+        project: './tsconfig.json',
+        tsconfigRootDir: import.meta.dirname,
         sourceType: 'module',
       },
     },
-    plugins: { import: importPlugin },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
     rules: {
-      ...importPlugin.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...tseslint.configs['recommended-type-checked'].rules,
+      ...tseslint.configs['stylistic-type-checked'].rules,
+      '@typescript-eslint/no-unused-vars': 'off',
     },
   },
-  ...compat.extends('airbnb-base'),
   {
-    rules: {
-      'no-underscore-dangle': [
-        'error',
-        {
-          allow: ['__filename', '__dirname'],
-        },
-      ],
-      'import/extensions': [
-        'error',
-        {
-          js: 'always',
-        },
-      ],
-      'import/no-named-as-default': 'off',
-      'import/no-named-as-default-member': 'off',
-      'no-console': 'off',
-      'import/no-extraneous-dependencies': 'off',
+    files: ['**/*.{js,cjs,mjs}'],
+    languageOptions: {
+      globals: globals.node,
     },
+  },
+  {
+    ignores: ['dist/'],
   },
 ];
