4008b1e99592df96e10ecaeb57786a4ff17aa371
olga rozmetova <olga.rozmetova@gmail.com>
2025-08-27 18:18:29 +0200

    refactor brain-gcd

--- src/games/brain-gcd.js
+++ src/games/brain-gcd.js
@@ -1,21 +1,25 @@
-import app from '../index.js'
-import getRandomNum from '../randomNum.js'
+import app from '../index.js';
+import getRandomNum from '../randomNum.js';
 
-const questionDescr = 'Find the greatest common divisor of given numbers. '
+const questionDescr = 'Find the greatest common divisor of given numbers. ';
 
-function getData() {
-  let num1 = getRandomNum()
-  let num2 = getRandomNum()
-  const question = `${num1} ${num2}`
-
+function calculate(num1, num2) {
   while (num2 !== 0) {
-    const temp = num2
-    num2 = num1 % num2
-    num1 = temp
+    const temp = num2;
+    num2 = num1 % num2;
+    num1 = temp;
   }
 
-  const correctAnswer = num1
-  return [question, correctAnswer.toString()]
+  return num1;
 }
 
-export default () => app(questionDescr, getData)
+function getData() {
+  let num1 = getRandomNum();
+  let num2 = getRandomNum();
+  const question = `${num1} ${num2}`;
+
+  const correctAnswer = calculate(num1, num2);
+  return [question, correctAnswer.toString()];
+}
+
+export default () => app(questionDescr, getData);
